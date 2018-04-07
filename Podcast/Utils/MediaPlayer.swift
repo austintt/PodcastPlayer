8814467e9759d9311d37dc8152e33bd8bf8f70b7
Austin Tooley <tooley7@gmail.com>
2018-04-07 15:27:29 -0500

    about to fix attribution

---
+++ Podcast/Utils/MediaPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  MediaPlayer.swift
+//  Podcast
+//
+//  Created by Austin Tooley on 3/1/18.
+//  Copyright © 2018 Austin Tooley. All rights reserved.
+//
+
+import Foundation
+
+class MediaPlayer {
+    static let sharedInstance = MediaPlayer()
+}
